9775dbb3a4dda642c3c2a4e36e92a69239d155e6
ljy <liangjy881@naver.com>
2021-12-29 05:24:01 -0800

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by ljy on 2021/12/29.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
